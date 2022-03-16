25acbb8c3df4fab90111a72e7e7b2938312828fa
Ross Coombs <2410466@dundee.ac.uk>
2022-03-16 11:06:38 +0000

    Made a sexy menu

--- ConsoleApplication3-1/ConsoleApplication3/ATM.cs
+++ ConsoleApplication3-1/ConsoleApplication3/ATM.cs
@@ -23,5 +23,25 @@ namespace ConsoleApplication3
         {
 
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
