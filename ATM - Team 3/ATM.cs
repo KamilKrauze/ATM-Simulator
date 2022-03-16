f31e8c1241fb965e6a32845a8060707ae5045328
Kamil Krauze <72396466+KamilKrauze@users.noreply.github.com>
2022-03-16 20:15:52 +0000

    Accounts fully imported

--- ATM - Team 3/ATM.cs
+++ ATM - Team 3/ATM.cs
@@ -12,11 +12,17 @@ namespace ATM_Team3
 {
     public partial class ATM_form : Form
     {
+        private Account[] accounts = new Account[3];
+
+
         public ATM_form()
         {
+            accounts[0] = new Account(300, 1111, 111111);
+            accounts[1] = new Account(750, 2222, 222222);
+            accounts[2] = new Account(3000, 3333, 333333);
+
             InitializeComponent();
             this.Text = "ATM";
-
         }
 
         private void Form1_Load(object sender, EventArgs e)
