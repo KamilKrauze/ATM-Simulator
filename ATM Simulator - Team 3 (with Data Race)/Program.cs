dbde771787ba2a1ff3b2e3a1e35a928cda1e32c7
Kamil Krauze <72396466+KamilKrauze@users.noreply.github.com>
2022-03-22 15:16:28 +0000

    Constructor setup
    Executable generates

--- ATM Simulator - Team 3 (with Data Race)/Program.cs
+++ ATM Simulator - Team 3 (with Data Race)/Program.cs
@@ -6,17 +6,23 @@ using System.Windows.Forms;
 
 namespace ATM_Simulator___Team_3__with_Data_Race_
 {
-    static class Program
+    public class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+        private Account[] account = new Account[3];
+
+        public Program()
+        {
+            account[0] = new Account(300, 1111, 111111);
+            account[1] = new Account(750, 2222, 222222);
+            account[2] = new Account(3000, 3333, 333333);
+        }
         [STAThread]
         static void Main()
         {
+            Program program = new Program();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ATM_form());
+            Application.Run(new ATM_form(program.account));
         }
     }
 }
